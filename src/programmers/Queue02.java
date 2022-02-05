9095c8ec48833aebc1dbdcf541fa43085cb4fd22
dayeon730 <bdy730@nate.com>
2022-02-05 12:01:53 +0900

    프로그래머스 완주하지못한선수

--- src/programmers/Queue02.java
+++ src/programmers/Queue02.java
@@ -3,34 +3,31 @@ package programmers;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-
+//프로그래머스 해시 > 완주하지 못한 선수 
 public class Queue02 {
 
 	public static void main(String[] args) {
 		class Solution {
 		    public String solution(String[] participant, String[] completion) {
-		        String answer = "";
-		        Queue<String > queue = new LinkedList<>();
-		       Arrays.sort(participant);
-		       Arrays.sort(completion);
-		       
-		       for (int i = 0; i <participant.length; i++) {
-		    	  queue.add(participant[i]);
-			}
-		       for (int j = 0; j < participant.length; j++) {
-		    	   String ans = queue.remove();
-		    	   if(j<completion.length && !ans.equals(completion[j]) ) {
-		    		 //  System.out.println(ans);
-		    		   answer = ans; 
-		    		   break;
-		    	   }else {
-		    		  // System.out.println(participant[participant.length-1]);
-		    	  answer = participant[participant.length-1];
-		    	  System.out.println("2");
-		    	   }
-			}
-		       System.out.println(answer);
-		        return answer;
+		    	 String answer = "";
+		 		Queue<String > queue = new LinkedList<>();
+		 		Arrays.sort(participant);
+		 		Arrays.sort(completion);
+		 		       
+		 		for (int i = 0; i <participant.length; i++) {
+		 		 queue.add(participant[i]);
+		 		}
+		 		for (int j = 0; j < completion.length; j++) {
+		 		  String ans = queue.remove();
+		            if(!ans.equals(completion[j]) ) {
+		 		    answer = ans; 
+		 		    break;
+		 		   }else {
+		 		     answer = participant[participant.length-1];
+		 		   }
+		 		}
+		 		System.out.println(answer);
+		 		 return answer;
 		    }
 		}
 		
